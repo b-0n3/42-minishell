a60695bf0b2c298488d9d8aaa501e8798fd95335
Ayoub M'khantar <am-khant@e2r6p10.1337.ma>
2021-12-07 04:09:02 +0100

    added files is done

--- src/parse_word.c
+++ src/parse_word.c
@@ -3,60 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   parse_word.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aait-ham <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: am-khant <am-khant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/07 03:38:00 by aait-ham          #+#    #+#             */
-/*   Updated: 2021/12/07 03:38:02 by aait-ham         ###   ########.fr       */
+/*   Created: 2021/12/07 03:43:48 by am-khant          #+#    #+#             */
+/*   Updated: 2021/12/07 03:45:58 by am-khant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void parse_word_pipeline(t_node *head, t_node *com)
+void	parse_word_pipeline(t_node *head, t_node *com)
 {
-    if (head->left == NULL) {
-                head->left = com;
-                com->isleft = TRUE;
-            }
-              else {
-                head->right = com;
-                com->isleft = FALSE;
-            }
-                com->parent = head;
-}
-t_node  *parse_args(t_node *head, t_token *token, t_node *com)
-{
-    while (1)
+	if (head->left == NULL)
 	{
-			token = pre_get_next_token(this);
-            if (token != NULL)
-               token->expand(token,this->exit_code, this->env);
-			if (token == NULL || token->type != word)
-				return handle_operator(this, token ,head);
-			com->args.push(&com->args, token , sizeof(t_token));
-		}
-    return head;
+		head->left = com;
+		com->isleft = TRUE;
+	}
+	else
+	{
+		head->right = com;
+		com->isleft = FALSE;
+	}
+		com->parent = head;
 }
 
-t_node *handle_word(t_shell *this, t_token *token ,t_node *head)
+t_node	*parse_args(t_node *head, t_token *token, t_node *com)
 {
-	t_node *com;
+	while (1)
+	{
+		token = pre_get_next_token(this);
+		if (token != NULL)
+			token->expand(token, this->exit_code, this->env);
+		if (token == NULL || token->type != word)
+			return (handle_operator(this, token, head));
+		com->args.push(&com->args, token, sizeof(t_token));
+	}
+	return (head);
+}
 
-    token->expand(token,this->exit_code, this->env);
+t_node	*handle_word(t_shell *this, t_token *token, t_node *head)
+{
+	t_node	*com;
+
+	token->expand(token, this->exit_code, this->env);
 	if (head != NULL && head->right != NULL
-    && head->right->need_a_file(head->right))
-        add_file(this,head->right, token);
+		&& head->right->need_a_file(head->right))
+		add_file(this, head->right, token);
 	else if (head != NULL && head->need_a_file(head))
-		add_file(this,head, token);
-	else if (head == NULL || head->op_type == pipeline  )
+		add_file(this, head, token);
+	else if (head == NULL || head->op_type == pipeline)
 	{
 		com = token->to_node(token);
 		token->free(token);
 		if (head != NULL)
-            parse_word_pipeline(head, com);
-        else
+			parse_word_pipeline(head, com);
+		else
 			head = com;
-		return (parse_args(head, token ,com));
+		return (parse_args(head, token, com));
 	}
-	return head;
+	return (head);
 }
--- src/token.c
+++ src/token.c
@@ -3,56 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   token.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aait-ham <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: am-khant <am-khant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/07 03:28:04 by aait-ham          #+#    #+#             */
-/*   Updated: 2021/12/07 03:28:06 by aait-ham         ###   ########.fr       */
+/*   Created: 2021/12/07 03:47:05 by am-khant          #+#    #+#             */
+/*   Updated: 2021/12/07 03:52:47 by am-khant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_bool has_next_token(t_shell *this)
+t_bool	has_next_token(t_shell *this)
 {
-	return this->cursor <this->command_len;
+	return (this->cursor < this->command_len);
 }
 
-t_token *pre_get_next_token(t_shell *this)
+t_token	*pre_get_next_token(t_shell *this)
 {
-	t_token *token;
+	t_token	*token;
 
 	token = this->get_next_token(this);
-	return token;
+	return (token);
 }
 
-void *str_to_token(void *str)
+void	*str_to_token(void *str)
 {
-    return new_token(strdup((t_string )str), word);
+	return (new_token(ft_strdup((t_string)str), word));
 }
 
-t_node *token_to_node(t_token *this)
+t_bool	nodecreate(t_node *node, t_token *this)
 {
-	t_node  *node;
-    t_array_iterator  *iter;
-
 	node = new_node();
 	if (node == NULL)
+		return (FALSE);
+	node->value = ft_strdup(this->value);
+	return (TRUE);
+}
+
+t_node	*token_to_node(t_token *this)
+{
+	t_node				*node;
+	t_array_iterator	*iter;
+
+	if (!nodecreate(node, this))
 		return (NULL);
-	node->value = strdup(this->value);
-	if (this->type == word) {
-        node->word_type = command;
-        if (this->start_with != '\"' && this->start_with != '\'') {
-            iter = split(this->value, ' ');
-            if (iter->list->index > 1) {
-                free(node->value);
-                node->value = strdup((char *)iter->next(iter));
-                while(iter->has_next(iter))
-                  node->args.push(&node->args, iter->do_on_next(iter, &str_to_token), sizeof(t_token));
-            }
-            iter->free(iter, &free);
-        }
-    }
-    else
+	if (this->type == word)
+	{
+		node->word_type = command;
+		if (this->start_with != '\"' && this->start_with != '\'')
+		{
+			iter = split(this->value, ' ');
+			if (iter->list->index > 1)
+			{
+				free(node->value);
+				node->value = ft_strdup((char *)iter->next(iter));
+				while (iter->has_next(iter))
+					node->args.push(&node->args,
+						iter->do_on_next(iter, &str_to_token), sizeof(t_token));
+			}
+			iter->free(iter, &free);
+		}
+	}
+	else
 		node->op_type = find_operation_type(this->value);
-	return node;
+	return (node);
 }
