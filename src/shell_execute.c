db8921b386a2d8b0e8683499e8422670f962747b
Ayoub M'khantar <am-khant@e2r6p10.1337.ma>
2021-12-07 06:03:38 +0100

    norm done

--- src/shell_execute.c
+++ src/shell_execute.c
@@ -3,20 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   shell_execute.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aait-ham <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: am-khant <am-khant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 05:58:32 by aait-ham          #+#    #+#             */
-/*   Updated: 2021/12/07 05:58:36 by aait-ham         ###   ########.fr       */
+/*   Updated: 2021/12/07 06:03:16 by am-khant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-extern int g_mood;
 
-t_bool close_fds(t_shell *this, t_node *head)
+extern int	g_mood;
+
+t_bool	close_fds(t_shell *this, t_node *head)
 {
 	if (head == NULL)
-		return TRUE;
+		return (TRUE);
 	if (head->op_type == pipeline)
 	{
 		close(head->p[0]);
@@ -38,47 +39,51 @@ t_bool	wait_for_all(t_shell *this, t_node *head)
 
 	if (head == NULL)
 		return (TRUE);
-	if (head->word_type == command && this->last_one != head->pid) {
+	if (head->word_type == command && this->last_one != head->pid)
 		waitpid(head->pid, &exi, 0);
-	}
-	return wait_for_all(this,head->left) && wait_for_all(this, head->right);
+	return (wait_for_all(this, head->left) && wait_for_all(this, head->right));
 }
 
-t_bool is_built_in(t_string n)
+t_bool	is_built_in(t_string n)
 {
-	return (!ft_strcmp(n, "cd") || !ft_strcmp(n ,"pwd")  || !ft_strcmp(n ,"echo")
-			|| !ft_strcmp(n, "export") || !ft_strcmp(n, "env") || !ft_strcmp(n, "unset")
-			|| !ft_strcmp(n, "exit"));
+	return (!ft_strcmp(n, "cd") || !ft_strcmp(n, "pwd") || !ft_strcmp(n, "echo")
+		|| !ft_strcmp(n, "export") || !ft_strcmp(n, "env")
+		|| !ft_strcmp(n, "unset")
+		|| !ft_strcmp(n, "exit"));
 }
 
-t_exec_v *find_function(t_shell  *this , t_string value)
+t_exec_v	*find_function(t_shell  *this, t_string value)
 {
-	t_exec_v *func = (t_exec_v *)this->exec_pool.find_by_key(this->exec_pool, value);
-	if(func ==  NULL)
+	t_exec_v	*func;
+
+	func = (t_exec_v *)this->exec_pool.find_by_key(this->exec_pool, value);
+	if (func ==  NULL)
 		func = &exec_other;
-	return func;
+	return (func);
 }
 
-void  shell_execute(t_shell *this)
+void	shell_execute(t_shell *this)
 {
-	t_exec_v *fun;
+	t_exec_v	*fun;
 
 	g_mood = 1;
 	if (this->head == NULL)
 		return ;
 	exec_all_heredocs(this, this->head);
-	if ((this->head->parent == NULL || this->head->parent->need_a_file(this->head->parent))
-			&& is_built_in(this->head->value) )
+	if ((this->head->parent == NULL
+			|| this->head->parent->need_a_file(this->head->parent))
+		&& is_built_in(this->head->value))
 	{
 		g_mood = 2;
 		fun = find_function(this, this->head->value);
 		fun(this, this->head);
 		g_mood = 1;
-	}else {
+	}
+	else
+	{
 		launch(this, this->head);
 		wait_for_all(this, this->head);
 		waitpid(this->last_one, &this->exit_code, 0);
-
 	}
 	close_fds(this, this->head);
 	g_mood = 0;
